a544e4fa209d7d867c6682243800dd4a9e406551
Fauzan Dias Khairi <fauzandiaskhairi@gmail.com>
2026-07-06 09:49:10 +0700

    feat: implement laundry order management controller and home page view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\PesananController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/', function () {
+    return view('home');
+})->name('home');
