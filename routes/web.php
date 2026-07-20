e232c005e8c138b46a343abe7db79125c89430a4
Fauzan Dias Khairi <fauzandiaskhairi@gmail.com>
2026-07-20 20:34:31 +0700

    Pembuatan admin page

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CekCucianController;
 
-
+ 
 Route::view('/', 'home');
 Route::view('/home', 'home');
 Route::view('/profil', 'profil');
@@ -11,3 +11,7 @@ Route::view('/cabang', 'cabang');
 Route::view('/layanan', 'layanan');
 Route::get('/cek-cucian', [CekCucianController::class,'index']);
 Route::post('/cek-cucian', [CekCucianController::class,'cek'])->name('cek.cucian');
+
+ //Route untuk admin
+
+ 
